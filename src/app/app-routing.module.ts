d38e12e422a93d1af88ad2876a740e5303482fb3
Josephine <josephine.closan@yahoo.com>
2022-03-10 18:41:11 +0200

    finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PhoneDetailComponent } from './phone-detail/phone-detail.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PhoneListComponent } from './phone-list/phone-list.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/phone-list', pathMatch: 'full' },
+  
+  { path: 'phone-list', component: PhoneListComponent },
+  { path: 'details/:id', component: PhoneDetailComponent }
 
+];
 
 @NgModule({
   declarations: [],
+  exports: [RouterModule],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ]
 })
 export class AppRoutingModule { }
--- src/app/phone-list/phone-list.component.ts
+++ src/app/phone-list/phone-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PhoneService } from '../phone.service';
+import { Phones } from '../phone';
 
 @Component({
   selector: 'app-phone-list',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./phone-list.component.css']
 })
 export class PhoneListComponent implements OnInit {
+    title = 'Phone catalog';
+    productsList!: any[];
+    phones!: Phones[];
+    term: string = '';
+    selectedSort: string = '';
 
-  constructor() { }
+  constructor(
+    public phoneService: PhoneService
+  ) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.phoneService.getAllProducts().subscribe(
+      (data: any[]) => {
+        this.productsList = data;
+      },
+      (error: any) => {
+        console.log('http-error:');
+        console.log(error);
+      }
+    );
+  }
+  
+  sortBy(event: any) {
+    this.selectedSort = event.target.value;
+    if (this.selectedSort == 'name') {
+      this.productsList.sort(function (a, b) {
+       return a.name.localeCompare(b.name)
+      });
+    } else if (this.selectedSort == 'age') {
+      this.productsList.sort(function (a, b) {
+        return parseInt(a.age) - parseInt(b.age);
+      });
+    }
+    
   }
 
 }
